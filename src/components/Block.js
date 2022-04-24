9d1f6e9378b7403adeacddf31c296bd7a668d3f4
Burulai <96347574+burulaiplanet@users.noreply.github.com>
2022-04-24 16:33:10 +0600

    Update Block.js

--- src/components/Block.js
+++ src/components/Block.js
@@ -2,11 +2,11 @@ import './Block.css'
 
 function Block(props) {
     
-	if (props.block == 'yellow') {
+	if (props.block === 'yellow') {
 		return <div style={{ backgroundColor:"yellow" }}></div>
-	} else if (props.block == 'green') {
+	} else if (props.block === 'green') {
 		return <div style={{ backgroundColor:"green" }}></div>
-	} else if (props.block == 'color') {
+	} else if (props.block === 'color') {
 		return <div style={{ backgroundColor:'lime'}}></div>
 	} else {
 	 	return <div style={{ backgroundColor:'red' }}></div>
